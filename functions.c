64a501a3e3ac4effc520e3655837bfc031a23745
Christy Guan <cguan00@stuy.edu>
2019-12-04 10:49:04 -0500

    headers?

--- functions.c
+++ functions.c
@@ -1,2 +1,8 @@
 #include <stdio.h>
-#include "headers.h"
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <errno.h>
+#include <dirent.h>
