a2bcf2944f36eeb0317be06a21427ae5f85271d7
Christy Guan <cguan00@stuy.edu>
2019-12-04 10:41:51 -0500

    added include

--- headers.h
+++ headers.h
@@ -1,3 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <errno.h>
+#include <dirent.h>
+
 //MAIN FUNCTION
 int main(int argc, char **argv);
 
