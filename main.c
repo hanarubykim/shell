2b5017afe52c3d154ca5f73e76db58b261df517b
Hana Kim <hanarubykim2002@gmail.com>
2019-12-09 20:50:16 -0500

    redirection woooosh!

--- main.c
+++ main.c
@@ -21,7 +21,6 @@ char ** parse_args(char *line, char *delimiter){
   return args;
 }
 
-
 int run_commands(char *line){
   char * cmd = malloc(64 * sizeof(char));
   strcat(cmd, line);
@@ -42,7 +41,6 @@ int run_commands(char *line){
 
 }
 
-
 void semicolon(char *line){
   char ** separated = parse_args(line, ";");
   int i = 0;
@@ -53,6 +51,22 @@ void semicolon(char *line){
   }
 }
 
+int redir_input(char *line){
+  char ** separated = parse_args(line, "<");
+  int fd;
+  int i = 1;
+  while(separated[i] != NULL){
+    fd = open(separated[i], O_RDONLY);
+    dup2(fd, STDIN_FILENO);
+    if(fd == =1){
+      //error
+      printf("Uh oh! %s\n", sterror(errno));
+    }
+    close(fd);
+    i++;
+  }
+  return 0;
+}
 
 int main(int argc, char *argv[]){
   while(1){
