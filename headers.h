b23ae43d43ab357f597f871b02d71197c21a0494
Christy Guan <cguan00@stuy.edu>
2019-12-15 17:19:49 -0500

    rearrange

--- headers.h
+++ headers.h
@@ -9,11 +9,9 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-char ** parse_args(char *line, char *delimiter);
-
 char * fixws(char *arg);
 
-void semicolon(char *line);
+char ** parse_args(char *line, char *delimiter);
 
 void redir_input(char ** arr, int i);
 
@@ -23,4 +21,8 @@ char* strReplace(char* line, char target, char* newStr);
 
 void call_cd(char ** args);
 
+int pipey(char *args);
+
 int run_commands(char *line);
+
+void semicolon(char *line);
